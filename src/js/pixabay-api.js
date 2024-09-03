3b0450fa4a4b27e96f7984144d9f847a4b85280d
KostiantynLebediev <lebedievkostiantyn@gmail.com>
2024-09-03 17:41:06 +0100

    all update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export default async function searchImagesByQuery(query, page) {
+  const URL = 'https://pixabay.com/api/';
+  const API_KEY = '39117010-188e78dbd91dcd6bc7f235c58';
+
+  try {
+    const response = await axios.get(URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: 15,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    iziToast.error({
+      position: 'topRight',
+      message: `${error}`,
+    });
+  }
+}
